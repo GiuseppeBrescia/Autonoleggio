195f822f0a9d197e2df7179191ca87aa10737e56
Utente <Utente@Utente-PC>
2017-10-30 10:24:58 +0100

    Un'altra piccola modifica

--- src/main/java/it/team/util/LocalDateSerializer.java
+++ src/main/java/it/team/util/LocalDateSerializer.java
@@ -2,15 +2,23 @@ package it.team.util;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
-public class LocalDateSerializer extends JsonSerializer<LocalDate> {
+public class LocalDateSerializer extends StdSerializer<LocalDate> {
+
+    private static final long serialVersionUID = 1L;
+
+    public LocalDateSerializer(){
+        super(LocalDate.class);
+    }
+
     @Override
-    public void serialize(LocalDate arg0, JsonGenerator arg1, SerializerProvider arg2) throws IOException, JsonProcessingException {
-        arg1.writeString(arg0.toString());
+    public void serialize(LocalDate value, JsonGenerator gen, SerializerProvider sp) throws IOException, JsonProcessingException {
+        gen.writeString(value.format(DateTimeFormatter.ISO_LOCAL_DATE));
     }
 }
